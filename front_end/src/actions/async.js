c4660a245ac4cbc73f92881691501733709ccc29
Alan Covarrubias <alan.covarrubias@slalom.com>
2019-08-23 11:07:11 -0700

    actions

--- front_end/src/actions/async.js
+++ front_end/src/actions/async.js
@@ -1,14 +1,12 @@
-import fetch from 'cross-fetch'
-import { namespaceActions } from '../helpers/namespaceModule'
+import api from '../api'
+import actions from './database'
 import {
   selectSport,
-  selectSeasonId,
-  selectGameId,
   selectSeasonsFetched,
   selectGamesFetched,
   selectStatsFetched,
 } from '../selectors'
-import actions from './database'
+import { namespaceActions } from '../helpers/namespaceModule'
 
 export const fetchData = (match) => async (dispatch, getState) => {
   const { path, params: { seasonId, gameId } } = match
@@ -18,7 +16,7 @@ export const fetchData = (match) => async (dispatch, getState) => {
   const shouldSeasonsFetch = !selectSeasonsFetched(state)
   const shouldGamesFetch = !selectGamesFetched(state).includes(seasonId)
   const shouldStatsFetch = !selectStatsFetched(state).includes(gameId)
-  if (shouldSeasonsFetch && path === '/seasons') {
+  if (shouldSeasonsFetch) {
     dispatch(fetchSeasons(namespacedActions))
     dispatch(namespacedActions.seasonsFetched())
   } else if (path === '/seasons/:seasonId/games') {
@@ -28,6 +26,8 @@ export const fetchData = (match) => async (dispatch, getState) => {
       dispatch(namespacedActions.gamesFetched(seasonId))
     }
   } else if (path === '/seasons/:seasonId/games/:gameId') {
+    dispatch(namespacedActions.seasonId(seasonId))
+    dispatch(namespacedActions.gameId(gameId))
     if (shouldStatsFetch) {
       dispatch(fetchStats(namespacedActions))
       dispatch(namespacedActions.statsFetched(gameId))
@@ -36,19 +36,13 @@ export const fetchData = (match) => async (dispatch, getState) => {
 }
 
 const fetchSeasons = (actions) => async (dispatch, getState) => {
-  const state = getState()
-  const sport = selectSport(state)
-  const response = await fetch(`/${sport}/seasons`)
+  const response = await api.fetchSeasons(actions, getState())
   const json = await response.json()
-  const seasons = json.seasons
-  dispatch(actions.createSeasons(seasons))
+  dispatch(actions.createSeasons(json.seasons))
 }
 
 const fetchGames = (actions) => async (dispatch, getState) => {
-  const state = getState()
-  const sport = selectSport(state)
-  const seasonId = selectSeasonId(state)
-  const response = await fetch(`/${sport}/seasons/${seasonId}/games`)
+  const response = await api.fetchGames(actions, getState())
   const json = await response.json()
   dispatch(actions.createSeason(json.season))
   dispatch(actions.createTeams(json.teams))
@@ -56,17 +50,12 @@ const fetchGames = (actions) => async (dispatch, getState) => {
 }
 
 const fetchStats = (actions) => async (dispatch, getState) => {
-  const state = getState()
-  const sport = selectSport(state)
-  const seasonId = selectSeasonId(state)
-  const gameId = selectGameId(state)
-  const response = await fetch(`/${sport}/seasons/${seasonId}/games/${gameId}`)
+  const response = await api.fetchStats(actions, getState())
   const json = await response.json()
-  /*
-  dispatch(actions.createPlayers(json.away_players))
-  dispatch(actions.createPlayers(json.home_players))
-  dispatch(actions.createStats(json.away_stats))
-  dispatch(actions.createStats(json.home_stats))
-  */
+  dispatch(actions.createSeason(json.season))
+  dispatch(actions.createGame(json.game))
+  dispatch(actions.createTeams(json.teams))
+  dispatch(actions.createPlayers(json.players))
+  dispatch(actions.createStats(json.stats))
 }
 
---
+++ front_end/src/api/index.js
@@ -0,0 +1,14 @@
+import fetch from 'cross-fetch'
+import {
+  selectSport,
+  selectSeasonId,
+  selectGameId,
+} from '../selectors'
+
+const api = {
+  fetchSeasons: async (actions, state) => fetch(`/${selectSport(state)}/seasons`),
+  fetchGames: async (actions, state) => fetch(`/${selectSport(state)}/seasons/${selectSeasonId(state)}/games`),
+  fetchStats: async (actions, state) => fetch(`/${selectSport(state)}/seasons/${selectSeasonId(state)}/games/${selectGameId(state)}`),
+}
+
+export default api
